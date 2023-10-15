107ef9a77c61aeb917a401a0c4aeb94f4a3b46b7
Vadim Niziev <st1mpro23@yandex.ru>
2023-10-15 17:29:43 +0300

    разработка backend-a

---
+++ server/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    DbModule,
+    AuthModule,
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
